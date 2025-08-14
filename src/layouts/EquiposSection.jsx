4abfc54b4dd11be066f62d97efc5460a6572b661
NETNEO <rival.moro@hotmail.it>
2025-08-14 14:08:32 +0200

    equipos section for mobile finished

--- src/layouts/EquiposSection.jsx
+++ src/layouts/EquiposSection.jsx
@@ -1,9 +1,5 @@
 import s from "../assets/styles/layouts/equiposSection.module.css";
-import {
-  item,
-  childrenEquiposTwo,
-  childrenEquiposThree,
-} from "../assets/styles/components/introBox.module.css";
+import sIntroB from "../assets/styles/components/introBox.module.css";
 
 import { equipos } from "../data/headings";
 import TitleBox from "../components/TitleBox";
@@ -11,6 +7,8 @@ import IntroBox from "../components/IntroBox";
 import { equiposOne, equiposTwo, equiposThree } from "../data/introBoxes";
 import TrophyIcon from "../assets/icons/trophy.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
+import PhoneIcon from "../assets/icons/phone.svg?react";
+import MailIcon from "../assets/icons/mail.svg?react";
 
 export default function EquiposSection() {
   return (
@@ -20,15 +18,15 @@ export default function EquiposSection() {
         <IntroBox data={equiposOne} bg="black">
           <div>
             <ul>
-              <li className={item}>
+              <li className={sIntroB.item}>
                 <CheckIcon />
                 <span>Pasión</span>
               </li>
-              <li className={item}>
+              <li className={sIntroB.item}>
                 <CheckIcon />
                 <span>Compromiso</span>
               </li>
-              <li className={item}>
+              <li className={sIntroB.item}>
                 <CheckIcon />
                 <span>Excelencia</span>
               </li>
@@ -37,13 +35,13 @@ export default function EquiposSection() {
         </IntroBox>
 
         <IntroBox data={equiposTwo} bg="black">
-          <div className={childrenEquiposTwo}>
+          <div className={sIntroB.childrenEquiposTwo}>
             <TrophyIcon />
             <ul>
-              <li className={`${item} u-text-white`}>
+              <li className={`${sIntroB.item} u-text-white`}>
                 <span>2 Equipos Federados</span>
               </li>
-              <li className={`${item} u-text-white`}>
+              <li className={`${sIntroB.item} u-text-white`}>
                 <span>3 de SNP</span>
               </li>
             </ul>
@@ -51,7 +49,22 @@ export default function EquiposSection() {
         </IntroBox>
 
         <IntroBox data={equiposThree} bg="black">
-          <div className={childrenEquiposThree}></div>
+          <div className={sIntroB.childrenEquiposThree}>
+            <div className={sIntroB.contact}>
+              <span>Llamanos</span>
+              <div>
+                <PhoneIcon />
+                <a href="tel:628800205">628 800 205</a>
+              </div>
+            </div>
+            <div className={sIntroB.contact}>
+              <span>Escríbenos</span>
+              <div>
+                <MailIcon />
+                <a href="mailto:info@rdapadel.com">Info@rdapadel.com</a>
+              </div>
+            </div>
+          </div>
         </IntroBox>
       </div>
     </div>
